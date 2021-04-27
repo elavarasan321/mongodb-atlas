f39b967fcd761defb6bb402c0116739565da6dfd
elavarasan321 <elavarasan@bititude.com>
2021-04-27 16:20:40 +0530

    myfirst commit

--- server/app.js
+++ server/app.js
@@ -21,6 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 Routes(app);
+app.get("/",(res,req)=>{
+    res.render("index")
+} );
 
 module.exports = app;
 
